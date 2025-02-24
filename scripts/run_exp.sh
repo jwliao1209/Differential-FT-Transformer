18b026cd75017ccfb069abd79615f06d9d85872a
Jia-Wei-Liao <jw.liao1209@gmail.com>
2025-02-24 14:49:02 +0800

    [add] all exp

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
 data_ids=(
+    361055
+    361060
+    361061
+    361062
+    361065
+    361069
     361068
     361274
     361276
     361113
+    361072
+    361073
+    361074
+    361076
+    361077
+    361276
+    361097
+    361287
+)
+
+models=(
+    ft
+    diff
+    dint
 )
 
 for data_id in ${data_ids[@]}; do
-    python run.py \
-        --data_dir /home/jiawei/Desktop/github/DOFEN/tabular-benchmark/tabular_benchmark_data \
-        --data_id $data_id \
-        --model ftt \
-        --n_epoch 300 \
-        --target_transform
-
-    python run.py \
-        --data_dir /home/jiawei/Desktop/github/DOFEN/tabular-benchmark/tabular_benchmark_data \
-        --data_id $data_id \
-        --model dftt \
-        --n_epoch 300 \
-        --target_transform
+    for model in ${models[@]}; do
+        python run.py \
+            --data_dir /home/jiawei/Desktop/github/DOFEN/tabular-benchmark/tabular_benchmark_data \
+            --data_id $data_id \
+            --model $model \
+            --n_epoch 300 \
+            --target_transform
+    done
 done
