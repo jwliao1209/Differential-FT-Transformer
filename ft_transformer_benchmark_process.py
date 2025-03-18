0d20ec31dc09b8c5107a390f4968594bbb7f78e2
Jia-Wei-Liao <jw.liao1209@gmail.com>
2025-03-18 10:09:37 +0800

    [update] ft-bench preprocess

--- ft_transformer_benchmark_process.py
+++ ft_transformer_benchmark_process.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 import sklearn
 from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder, StandardScaler, QuantileTransformer
 
 
 def get_col_cat_count(data):
@@ -19,8 +20,8 @@ def get_col_cat_count(data):
 
 
 def main():
+    seed = 0
     datasets = os.listdir('data/')
-
     for dataset in tqdm(datasets):
         files = os.listdir(Path('data', dataset))
 
@@ -31,33 +32,64 @@ def main():
         test_categorical = np.load(Path('data', dataset, 'C_test.npy')) if 'C_test.npy' in files else None
         test_numerical = np.load(Path('data', dataset, 'N_test.npy')) if 'N_test.npy' in files else None
 
+        # Label encoding
+        if train_categorical is not None:
+            for i in range(train_categorical.shape[1]):
+                le = LabelEncoder()
+                train_categorical[:, i] = le.fit_transform(train_categorical[:, i])
+                valid_categorical[:, i] = le.transform(valid_categorical[:, i])
+                test_categorical[:, i] = le.transform(test_categorical[:, i])
+
+        if train_numerical is not None:
+            # Fill missing values with mean
+            col_mean = np.nanmean(train_numerical, axis=0)
+            inds = np.where(np.isnan(train_numerical))
+            train_numerical[inds] = np.take(col_mean, inds[1])
+
+            # Feature transformation
+            temp = train_numerical.copy()
+            if dataset in ["aloi", "helena"]:
+                transformer = StandardScaler()
+            else:
+                noise = 1e-3
+                stds = np.std(temp, axis=0, keepdims=True)
+                noise_std = noise / np.maximum(stds, noise)
+                temp += noise_std * np.random.default_rng(seed).standard_normal(temp.shape)
+                transformer = QuantileTransformer(
+                    output_distribution='normal',
+                    n_quantiles=max(min(temp.shape[0] // 30, 1000), 10),
+                    subsample=int(1e9),
+                    random_state=seed,
+                )
+            transformer.fit(temp)
+            train_numerical = transformer.transform(train_numerical)
+            valid_numerical = transformer.transform(valid_numerical)
+            test_numerical = transformer.transform(test_numerical)
+
         if train_categorical is not None and train_numerical is not None:
-            x_train = np.concatenate((train_categorical, train_numerical), axis=1)
-            x_valid = np.concatenate((valid_categorical, valid_numerical), axis=1)
-            x_test = np.concatenate((test_categorical, test_numerical), axis=1)
+            x_train = np.concatenate((train_categorical, train_numerical), axis=1).astype(np.float32)
+            x_valid = np.concatenate((valid_categorical, valid_numerical), axis=1).astype(np.float32)
+            x_test = np.concatenate((test_categorical, test_numerical), axis=1).astype(np.float32)
         elif train_categorical is not None:
-            x_train = train_categorical
-            x_valid = valid_categorical
-            x_test = test_categorical
+            x_train = train_categorical.astype(np.float32)
+            x_valid = valid_categorical.astype(np.float32)
+            x_test = test_categorical.astype(np.float32)
         elif train_numerical is not None:
-            x_train = train_numerical
-            x_valid = valid_numerical
-            x_test = test_numerical
+            x_train = train_numerical.astype(np.float32)
+            x_valid = valid_numerical.astype(np.float32)
+            x_test = test_numerical.astype(np.float32)
         else:
             raise ValueError('No data found')
 
-        y_train = np.load(Path('data', dataset, 'y_train.npy')).reshape(-1, 1)
-        y_valid = np.load(Path('data', dataset, 'y_val.npy')).reshape(-1, 1)
-        y_test = np.load(Path('data', dataset, 'y_test.npy')).reshape(-1, 1)
+        y_train = np.load(Path('data', dataset, 'y_train.npy'))
+        y_valid = np.load(Path('data', dataset, 'y_val.npy'))
+        y_test = np.load(Path('data', dataset, 'y_test.npy'))
 
-        transformer = sklearn.preprocessing.QuantileTransformer(
-            output_distribution='normal',
-            n_quantiles=max(min(y_train.shape[0] // 30, 1000), 10),
-            random_state=0,
-        )
-        y_train_transform = transformer.fit_transform(y_train.reshape(-1, 1))
-        y_valid_transform = transformer.transform(y_valid.reshape(-1, 1))
-        y_test_transform = transformer.transform(y_test.reshape(-1, 1))
+        # Target transformation
+        target_transformer = StandardScaler()
+        y_train_transform = target_transformer.fit_transform(y_train.reshape(-1, 1)).squeeze()
+        y_valid_transform = target_transformer.transform(y_valid.reshape(-1, 1)).squeeze()
+        y_test_transform = target_transformer.transform(y_test.reshape(-1, 1)).squeeze()
 
         info_path = Path('data', dataset, 'info.json')
         with open(info_path, 'r') as f:
